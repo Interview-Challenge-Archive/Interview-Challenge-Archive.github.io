7ac4f32d358bb5c9543ca03b59a16c6b9c5e58c1
Raimondas Rimkevičius (aka MekDrop) <github@mekdrop.name>
2026-03-23 21:44:18 +0200

    Use document-markdown-reader for task summary imports

--- src/utils/task-summary-importer.js
+++ src/utils/task-summary-importer.js
@@ -1,46 +1,35 @@
-const WORD_BINARY_TEXT_DECODER = new TextDecoder('latin1')
-const SUPPORTED_EXTENSIONS = new Set(['md', 'markdown', 'rtf', 'doc', 'docx', 'odt', 'fodt', 'txt'])
+import { documentMarkdownReader } from '@interview-challenge-archive/document-markdown-reader'
+import { marked } from 'marked'
 
-export const TASK_SUMMARY_IMPORT_ACCEPT = '.md,.markdown,.rtf,.doc,.docx,.odt,.fodt,.txt'
+marked.setOptions({
+  gfm: true,
+  breaks: true
+})
 
-let jsZipFactoryPromise
-let mammothFactoryPromise
-let markedFactoryPromise
+export const TASK_SUMMARY_IMPORT_ACCEPT = documentMarkdownReader.acceptedExtensions
 
 export async function importTaskSummaryFile (file) {
-  const extension = resolveFileExtension(file?.name)
-
-  if (!SUPPORTED_EXTENSIONS.has(extension)) {
-    throw new Error('unsupported-format')
+  if (isHtmlFile(file)) {
+    return sanitizeImportedHtml(await file.text())
   }
 
-  if (extension === 'md' || extension === 'markdown') {
-    return sanitizeImportedHtml(await renderMarkdownToHtml(await file.text()))
-  }
+  try {
+    const markdownContent = await documentMarkdownReader.readDocument(file)
+    const renderedContent = marked.parse(markdownContent)
+    const renderedHtml = typeof renderedContent === 'string'
+      ? renderedContent
+      : await renderedContent
 
-  if (extension === 'rtf') {
-    return plainTextToHtml(parseRtfToPlainText(await file.text()))
-  }
+    return sanitizeImportedHtml(renderedHtml)
+  } catch (error) {
+    const errorCode = String(error?.code ?? '').trim()
 
-  if (extension === 'docx') {
-    const mammoth = await loadMammothFactory()
-    const result = await mammoth.convertToHtml({ arrayBuffer: await file.arrayBuffer() })
-    return sanitizeImportedHtml(result.value)
-  }
+    if (errorCode) {
+      throw new Error(errorCode)
+    }
 
-  if (extension === 'odt') {
-    return sanitizeImportedHtml(await renderOdtArchiveToHtml(await file.arrayBuffer()))
+    throw error
   }
-
-  if (extension === 'fodt') {
-    return sanitizeImportedHtml(renderOpenDocumentXmlToHtml(await file.text()))
-  }
-
-  if (extension === 'doc') {
-    return plainTextToHtml(extractReadableWordBinaryText(await file.arrayBuffer()))
-  }
-
-  return plainTextToHtml(await file.text())
 }
 
 export function normalizeTextSummaryToHtml (value) {
@@ -57,145 +46,6 @@ export function normalizeTextSummaryToHtml (value) {
   return plainTextToHtml(normalizedValue)
 }
 
-function resolveFileExtension (fileName) {
-  const normalizedFileName = String(fileName ?? '').trim()
-  const lastDotIndex = normalizedFileName.lastIndexOf('.')
-
-  if (lastDotIndex <= -1 || lastDotIndex === normalizedFileName.length - 1) {
-    return ''
-  }
-
-  return normalizedFileName.slice(lastDotIndex + 1).toLowerCase()
-}
-
-async function renderMarkdownToHtml (markdownContent) {
-  const marked = await loadMarkedFactory()
-  const renderedContent = marked.parse(String(markdownContent ?? ''))
-  return typeof renderedContent === 'string'
-    ? renderedContent
-    : await renderedContent
-}
-
-function parseRtfToPlainText (rtfContent) {
-  const normalizedContent = String(rtfContent ?? '')
-    .replace(/\r\n?/g, '\n')
-    .replace(/\{\\fonttbl[\s\S]*?\}/gi, '')
-    .replace(/\{\\colortbl[\s\S]*?\}/gi, '')
-    .replace(/\{\\stylesheet[\s\S]*?\}/gi, '')
-    .replace(/\\par[d]?/g, '\n')
-    .replace(/\\line/g, '\n')
-    .replace(/\\tab/g, '\t')
-    .replace(/\\u-?\d+\??/g, '')
-    .replace(/\\'[0-9a-fA-F]{2}/g, (match) => String.fromCharCode(Number.parseInt(match.slice(2), 16)))
-    .replace(/\\([{}\\])/g, '$1')
-    .replace(/\\[a-z]+-?\d* ?/gi, '')
-    .replace(/[{}]/g, '')
-    .replace(/\n{3,}/g, '\n\n')
-
-  return normalizedContent.trim()
-}
-
-async function renderOdtArchiveToHtml (arrayBuffer) {
-  const JSZip = await loadJsZipFactory()
-  const archive = await JSZip.loadAsync(arrayBuffer)
-  const contentXmlFile = archive.file('content.xml')
-
-  if (!contentXmlFile) {
-    throw new Error('invalid-odt')
-  }
-
-  return renderOpenDocumentXmlToHtml(await contentXmlFile.async('string'))
-}
-
-function renderOpenDocumentXmlToHtml (xmlContent) {
-  const parser = new DOMParser()
-  const xmlDocument = parser.parseFromString(String(xmlContent ?? ''), 'application/xml')
-
-  if (xmlDocument.querySelector('parsererror')) {
-    throw new Error('invalid-odt')
-  }
-
-  const textRoot = xmlDocument.getElementsByTagName('office:text')[0] ?? xmlDocument.documentElement
-  const htmlBlocks = []
-
-  for (const childNode of Array.from(textRoot.children)) {
-    if (childNode.localName === 'h') {
-      htmlBlocks.push(`<h3>${escapeHtml(extractNodeText(childNode))}</h3>`)
-      continue
-    }
-
-    if (childNode.localName === 'p') {
-      htmlBlocks.push(`<p>${escapeHtml(extractNodeText(childNode))}</p>`)
-      continue
-    }
-
-    if (childNode.localName === 'list') {
-      const listItems = Array.from(childNode.children)
-        .filter((item) => item.localName === 'list-item')
-        .map((item) => `<li>${escapeHtml(extractNodeText(item))}</li>`)
-        .join('')
-
-      if (listItems) {
-        htmlBlocks.push(`<ul>${listItems}</ul>`)
-      }
-    }
-  }
-
-  if (!htmlBlocks.length) {
-    return plainTextToHtml(String(textRoot.textContent ?? ''))
-  }
-
-  return htmlBlocks.join('')
-}
-
-function extractNodeText (node) {
-  if (!node) {
-    return ''
-  }
-
-  let extractedText = ''
-
-  for (const childNode of Array.from(node.childNodes)) {
-    if (childNode.nodeType === Node.TEXT_NODE) {
-      extractedText += childNode.textContent ?? ''
-      continue
-    }
-
-    if (childNode.nodeType !== Node.ELEMENT_NODE) {
-      continue
-    }
-
-    if (childNode.localName === 'line-break') {
-      extractedText += '\n'
-      continue
-    }
-
-    if (childNode.localName === 'tab') {
-      extractedText += '\t'
-      continue
-    }
-
-    extractedText += extractNodeText(childNode)
-  }
-
-  return extractedText.replace(/\s+/g, ' ').trim()
-}
-
-function extractReadableWordBinaryText (arrayBuffer) {
-  const rawContent = WORD_BINARY_TEXT_DECODER.decode(new Uint8Array(arrayBuffer))
-  const normalizedContent = rawContent
-    .replace(/[^\x20-\x7E\r\n\t]+/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/\n{3,}/g, '\n\n')
-    .trim()
-
-  if (!normalizedContent) {
-    throw new Error('unreadable-doc')
-  }
-
-  return normalizedContent
-}
-
 function plainTextToHtml (text) {
   const normalizedText = String(text ?? '')
     .replace(/\r\n?/g, '\n')
@@ -209,15 +59,6 @@ function plainTextToHtml (text) {
     .split(/\n{2,}/)
     .map((paragraph) => `<p>${escapeHtml(paragraph).replace(/\n/g, '<br>')}</p>`)
     .join('')
-}
-
-function escapeHtml (value) {
-  return String(value ?? '')
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;')
 }
 
 function sanitizeImportedHtml (value) {
@@ -238,33 +79,26 @@ function sanitizeImportedHtml (value) {
     .trim()
 }
 
-async function loadJsZipFactory () {
-  if (!jsZipFactoryPromise) {
-    jsZipFactoryPromise = import('jszip').then((module) => module.default)
-  }
-
-  return jsZipFactoryPromise
+function escapeHtml (value) {
+  return String(value ?? '')
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;')
 }
 
-async function loadMammothFactory () {
-  if (!mammothFactoryPromise) {
-    mammothFactoryPromise = import('mammoth/mammoth.browser').then((module) => module.default)
+function isHtmlFile (file) {
+  const normalizedMimeType = String(file?.type ?? '')
+    .trim()
+    .toLowerCase()
+    .split(';')[0]
+
+  if (normalizedMimeType === 'text/html' || normalizedMimeType === 'application/xhtml+xml') {
+    return true
   }
 
-  return mammothFactoryPromise
-}
+  const normalizedFileName = String(file?.name ?? '').trim().toLowerCase()
 
-async function loadMarkedFactory () {
-  if (!markedFactoryPromise) {
-    markedFactoryPromise = import('marked').then((module) => {
-      module.marked.setOptions({
-        gfm: true,
-        breaks: true
-      })
-
-      return module.marked
-    })
-  }
-
-  return markedFactoryPromise
+  return normalizedFileName.endsWith('.html') || normalizedFileName.endsWith('.htm')
 }
